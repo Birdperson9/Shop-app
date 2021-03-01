0cc570eb4078854e758d501872ef0633effad8ff
Yevhenii <yorieshkin@gmail.com>
2021-03-01 21:35:54 +0200

    adding errorMiddleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,9 @@ import asyncHandler from 'express-async-handler'
 const router = express.Router()
 import Product from '../models/productModel.js'
 
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
 router.get(
   '/',
   asyncHandler(async (req, res) => {
@@ -12,6 +15,9 @@ router.get(
   })
 )
 
+// @desc Fetch single product
+// @route GET /api/products/:id
+// @access Public
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
@@ -19,7 +25,8 @@ router.get(
     if (product) {
       res.json(product)
     } else {
-      res.status(404).json({ message: 'Product not found' })
+      res.status(404)
+      throw new Error('Product not found')
     }
   })
 )
