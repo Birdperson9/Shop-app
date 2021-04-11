66fb066f42aefdc3ecb0f236ee5472aa7426c118
Yevhenii <yorieshkin@gmail.com>
2021-04-11 21:43:50 +0300

    add Morgan & createReview endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,10 +6,12 @@ import {
   deleteProduct,
   createProduct,
   updateProduct,
+  createProducReview,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProducReview)
 router
   .route('/:id')
   .get(getProductById)
